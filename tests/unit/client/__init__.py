dd15dbdffcdec7192db7bc5f094ed61c64c8e8c8
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-08-04 21:47:45 +0200

    inject token in test client

--- tests/unit/client/__init__.py
+++ tests/unit/client/__init__.py
@@ -1,4 +1,9 @@
+import os
+
 from anna_client.client import Client
 
 
 client = Client(endpoint='https://api.annahub.se')
+
+if 'ANNA_TOKEN' in os.environ:
+	client.inject_token(os.environ['ANNA_TOKEN'])
