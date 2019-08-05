669e69d4df8de14447d1250a3a1e6852d182b10d
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-08-05 23:05:22 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
 	name='anna-client',
-	version='1.1.2',
+	version='1.1.3',
 	author='Patrik Pihlstrom',
 	author_email='patrik.pihlstrom@gmail.com',
 	url='https://github.com/patrikpihlstrom/anna-client',
